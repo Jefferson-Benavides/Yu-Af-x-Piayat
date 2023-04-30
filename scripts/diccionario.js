6d56e080bdba81d23bf4591ccf4dc95442436ee8
Jefferson Benavides <82610127+Jefferson-Benavides@users.noreply.github.com>
2023-04-30 17:28:09 -0500

    Ejemplo con localCompare

---
+++ scripts/diccionario.js
@@ -0,0 +1,6 @@
+var items = ['réservé', 'premier', 'cliché', 'communiqué', 'café', 'adieu'];
+items.sort(function (a, b) {
+  return a.localeCompare(b);
+});
+
+// items is ['adieu', 'café', 'cliché', 'communiqué', 'premier', 'réservé']

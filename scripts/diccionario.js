41b538459ac1dcc507e9dcc0276b9374bbc70aad
Jefferson-Benavides <jjbenavidesoviedo@hotmail.com>
2023-05-03 05:54:09 -0500

    Página de bienvenida con about blank, ejercicio de sinónimos con palabras desconocidas

--- scripts/diccionario.js
+++ scripts/diccionario.js
@@ -1,6 +1,11 @@
-var items = ['artritis', 'árbol', 'réservé', 'premier', 'cliché', 'communiqué', 'café', 'adieu'];
-items.sort(function (a, b) {
-  return a.localeCompare(b);
-});
+let divEtradasDiccionario = document.getElementById('entradasdiccionario');
+
+
+divEtradasDiccionario.appendChild("<p>alert('Soy Juan con una alerta molesta!')</p>");
+
+// var items = ['artritis', 'árbol', 'réservé', 'premier', 'cliché', 'communiqué', 'café', 'adieu'];
+// items.sort(function (a, b) {
+//   return a.localeCompare(b);
+// });
 
 // items is ['adieu', 'café', 'cliché', 'communiqué', 'premier', 'réservé']

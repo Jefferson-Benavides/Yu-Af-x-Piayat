4249f38e7eedef985ff1e8f29756b5dd2ee8f950
Jefferson-Benavides <jjbenavidesoviedo@hotmail.com>
2023-05-10 21:55:11 -0500

    Caloto, reportajes NY

---
+++ plan-estudios/septimo/ii/temas/narrativos.js
@@ -0,0 +1,6 @@
+console.log('Yes');
+let divTextoJavascript = document.getElementById('texto-desde-javascript')
+
+let p = '<p>Uso de createElement!</p>';
+console.log(p);
+divTextoJavascript.appendChild(p);
